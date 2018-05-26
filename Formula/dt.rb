c59063129389e65f6b67de3628b18b87bb1113e6
goreleaserbot <goreleaser@carlosbecker.com>
2018-05-26 14:20:13 +0200

    Brew formula update for dt version v1.0.3

--- Formula/dt.rb
+++ Formula/dt.rb
@@ -1,9 +1,9 @@
 class Dt < Formula
   desc "CLI tool for common dev tasks."
   homepage "https://github.com/chclaus/dt"
-  url "https://github.com/chclaus/dt/releases/download/v1.0.2/dt_1.0.2_darwin_amd64.tar.gz"
-  version "1.0.2"
-  sha256 "04e3344beaa5b8f5f678b1e8292674a926a73189e497c81eeb69ed7c4f8c45d8"
+  url "https://github.com/chclaus/dt/releases/download/v1.0.3/dt_1.0.3_darwin_amd64.tar.gz"
+  version "1.0.3"
+  sha256 "ffc88837c3d247293c34ef8f3c4db5904afee8631513afa53b211d19cfd940c1"
 
   def install
     bin.install "dt"
